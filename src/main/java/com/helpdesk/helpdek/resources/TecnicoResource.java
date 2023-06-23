a89a96e51709a2a2323fa1846b025eda72b2aab9
Igor Buosi <igorbiancobuosi@hotmail.com>
2023-06-22 22:40:01 -0300

    endpoint findAll para tecnico

--- src/main/java/com/helpdesk/helpdek/resources/TecnicoResource.java
+++ src/main/java/com/helpdesk/helpdek/resources/TecnicoResource.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
@@ -20,9 +23,16 @@ public class TecnicoResource {
     private TecnicoService tecnicoService;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = tecnicoService.findById(id);
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
 
+    }
+
+    @GetMapping // sem nenhum parametro
+    public ResponseEntity<List<TecnicoDTO>> findAll() {
+        List<Tecnico> list = tecnicoService.findAll();
+        List<TecnicoDTO> listDto = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
--- src/main/java/com/helpdesk/helpdek/services/TecnicoService.java
+++ src/main/java/com/helpdesk/helpdek/services/TecnicoService.java
@@ -6,6 +6,7 @@ import com.helpdesk.helpdek.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,4 +19,7 @@ public class TecnicoService {
         return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id:" + id));
     }
 
+    public List<Tecnico> findAll() {
+        return tecnicoRepository.findAll();
+    }
 }
